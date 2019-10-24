ecafe5b501d3b789198e72597633091fab5e7c9b
Quirin Ertz <quirin.ertz@gmail.com>
2019-10-24 21:03:01 +0800

    Implemented LocationIPGeolocationIO

--- crawler/src/main/java/LocationService/LocationIPGeolocationIO.java
+++ crawler/src/main/java/LocationService/LocationIPGeolocationIO.java
@@ -1,16 +1,22 @@
 package LocationService;
 
-/*Max 1000/day*/
+import org.json.JSONObject;
+
+/*Max 1000/day https://api.ipgeolocation.io/ipgeo?apiKey=298fc8e3058d4c21a028c55f82608196&ip=1.1.1.1*/
 public class LocationIPGeolocationIO implements LocationServiceInterface {
 
+  private static final String KEY = "298fc8e3058d4c21a028c55f82608196";
+  private static final String host = "https://api.ipgeolocation.io/ipgeo";
 
   @Override
   public String getRequest (String ip) {
-    return null;
+    return String.format ("%s?apiKey=%s&ip=%s", host, KEY, ip);
   }
 
   @Override
   public Location getLocation (String data) {
-    return null;
+    JSONObject jsonData = new JSONObject (data);
+    return new Location (jsonData.getString ("ip"), jsonData.getString ("country_name"),
+                         jsonData.getDouble ("latitude"), jsonData.getDouble ("longitude"));
   }
 }
