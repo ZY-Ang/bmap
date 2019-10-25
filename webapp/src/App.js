e9f2c943c7f0373a20943ce1eb90ca4710d1024d
Alex Ang <E0032221@u.nus.edu>
2019-10-25 19:54:14 +0800

    Frontend WIP

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactMapGL, {Feature, Layer} from 'react-map-gl';
+import WorldMap from 'datamaps/dist/datamaps.all';
+import SgpMap from 'datamaps/dist/datamaps.sgp';
+import data from './data.json';
+
+// const populationByCountry = d3.json(worldMap);
+// const populationsByCountry = d3.tsvParse(data);
+
+// const options = {projectionKey: 'NaturalEarth'};
+// use defaults
+// optionsList.forEach((options) => {
+//     output(`./example/output-${options.projectionKey}`, worldMap(populationsByCountry, options))
+// });
+
 
 class App extends React.Component {
+    state = {
+        viewport: {},
+    };
+
+    componentDidMount() {
+        console.log(WorldMap.prototype.worldTopo);
+        console.log(SgpMap.prototype.sgpTopo);
+        // console.log(DataMap.prototype.canTopo);
+    }
+
+    onViewportChange = viewport => {
+        const {width, height, ...etc} = viewport;
+        this.setState({viewport: etc});
+    };
+
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                <div className="SearchBar"/>
+                <ReactMapGL
+                    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                    {...this.state.viewport}
+                    width='100%'
+                    height='100%'
+                    onViewportChange={viewport => this.onViewportChange(viewport)}
+                >
+                    <Layer type="heatmap" paint={null}>
+                        {
+                            data.map((element, index) => (
+                                <Feature key={index} coordinates={element.latlng} properties={element}/>
+                            ))
+                        }
+                    </Layer>
+                </ReactMapGL>
             </div>
         );
     }
