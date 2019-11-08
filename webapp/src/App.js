74e27335be9d34d0a1cf700063d23ba4658b15e9
Stephan Schiller <StephanSchiller@Stephans-MacBook-Pro-4.local>
2019-11-08 14:38:19 +0800

    added Heatmap

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
+import { scaleLinear } from "d3-scale";
+import { ComposableMap, Geographies, Geography, Sphere, Graticule } from "react-simple-maps";
+//import {getWeightedDataForWordOnce} from './database';
+
+const geoUrl =
+    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+const colorScale = scaleLinear()
+    .domain([0.29, 0.68])
+    .range(["#ffedea", "#ff5233"]);
+
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                <ComposableMap projectionConfig={{
+                    rotate: [-10, 0, 0],
+                    scale: 140
+                }}>
+                    <Sphere stroke="#E4E5E6" strokeWidth={1} />
+                    <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+                    <Geographies geography={geoUrl}>
+                        {({ geographies }) =>
+                            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+                        }
+                        
+                    </Geographies>
+                </ComposableMap>
             </div>
         );
     }
