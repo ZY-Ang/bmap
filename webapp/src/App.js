498fd422aeb0855a4cf783d89d7e10a67e22e541
Stephan Schiller <StephanSchiller@Stephans-MacBook-Pro-4.local>
2019-11-10 17:20:06 +0800

    change

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -104,7 +104,7 @@ class App extends React.Component {
     render() {
 
         console.log(this.state.wordDistribution);
-        let x = this.state.wordDistribution;
+
         return (
             <div className="App">
                 <form
